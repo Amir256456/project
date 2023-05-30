4970ff3b4a5fd88270ddf9cc583fcf0fd9846358
Amir256456 <121805189+torgoMarket@users.noreply.github.com>
2023-05-30 14:21:44 +0500

    fix2

--- script.js
+++ script.js
@@ -14,7 +14,15 @@ for (let smoothLink of smoothLinks) {
 
 let toggleBtn = document.querySelector('.toggleBtn');
 let nav = document.querySelector('.nav');
+let links = document.querySelectorAll('.link');
 
+links.forEach(element => {
+    element.addEventListener('click', () =>{
+        toggleBtn.classList.remove('fa-xmark');
+        toggleBtn.classList.add('fa-bars');
+        nav.classList.toggle('active');
+    })
+})
 toggleBtn.addEventListener('click', () =>{
     if(toggleBtn.classList.contains('fa-bars')){
         toggleBtn.classList.remove('fa-bars');
